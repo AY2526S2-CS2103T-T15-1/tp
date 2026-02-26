e860e326fee6e14510c40051f82bed451c6066b1
Ananya Jain <ananyaj1515@gmail.com>
2026-02-26 18:40:28 +0800

    Follow add command tutorial

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,50 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+public class Remark {
+    public static final String MESSAGE_CONSTRAINTS = "Remark can take any values, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Remark}.
+     *
+     * @param remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherRemark = (Remark) other;
+        return value.equals(otherRemark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
