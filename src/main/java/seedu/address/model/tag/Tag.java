60899824222abf23c9299b4dc032a5a3939f6878
pr8190 <E1475630@u.nus.edu>
2026-03-11 20:33:45 +0800

    Update EditCommand

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -3,6 +3,8 @@ package seedu.address.model.tag;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.util.Objects;
+
 /**
  * Represents a Tag in the address book.
  * Guarantees: immutable; name is valid as declared in {@link #isValidTagName(String)}
@@ -54,14 +56,11 @@ public class Tag {
         if (other == this) {
             return true;
         }
-
-        // instanceof handles nulls
         if (!(other instanceof Tag)) {
             return false;
         }
-
         Tag otherTag = (Tag) other;
-        return type.equals(otherTag.type) && tagName.equals(otherTag.tagName);
+        return Objects.equals(type, otherTag.type) && tagName.equals(otherTag.tagName);
     }
 
     @Override
