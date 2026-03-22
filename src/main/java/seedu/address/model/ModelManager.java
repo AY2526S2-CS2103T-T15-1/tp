3cb57fa9a53993a944f5754ffabda8c743efe3e1
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-22 16:35:25 +0800

    Model: Add FilterDetails encapsulation

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import seedu.address.commons.core.GuiSettings;
@@ -23,6 +25,7 @@ public class ModelManager implements Model {
     private final AddressBook addressBook;
     private final UserPrefs userPrefs;
     private final FilteredList<Person> filteredPersons;
+    private final ObjectProperty<FilterDetails> filterDetails = new SimpleObjectProperty<>(new FilterDetails());
 
     /**
      * Initializes a ModelManager with the given addressBook and userPrefs.
@@ -133,6 +136,13 @@ public class ModelManager implements Model {
     public void updateFilteredPersonList(Predicate<Person> predicate) {
         requireNonNull(predicate);
         filteredPersons.setPredicate(predicate);
+    }
+
+    //=========== Filter Details Accessors =============================================================
+
+    @Override
+    public ObjectProperty<FilterDetails> getFilterDetailsProperty() {
+        return filterDetails;
     }
 
     @Override
