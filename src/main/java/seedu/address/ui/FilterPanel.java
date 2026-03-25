081b5b081c8b66c01ed30581de7fb208ee53bafe
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-25 16:31:41 +0800

    Update FilterPanel to use executor

--- src/main/java/seedu/address/ui/FilterPanel.java
+++ src/main/java/seedu/address/ui/FilterPanel.java
@@ -1,57 +1,37 @@
 package seedu.address.ui;
 
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
-import org.kordamp.ikonli.javafx.FontIcon;
-
-import javafx.beans.property.ObjectProperty;
+import javafx.collections.SetChangeListener;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
-import seedu.address.commons.util.StringUtil;
+import javafx.scene.layout.StackPane;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.FilterDetails;
-import seedu.address.ui.filter.FilterPanelTag;
+import seedu.address.model.ReadOnlyFilterDetails;
+import seedu.address.ui.executors.FilterExecutor;
+import seedu.address.ui.filter.FilterPanelField;
 
 /**
  * Panel containing the list of filtering and sorting options.
  */
 public class FilterPanel extends UiPart<Region> {
     private static final String FXML = "FilterPanel.fxml";
-    private final ObjectProperty<FilterDetails> filterDetails;
+    private final ReadOnlyFilterDetails filterDetails;
+    private final FilterExecutor filterExecutor;
 
     @FXML
-    private TextField nameFilterField;
-    @FXML
-    private FlowPane nameTagsFlowPane;
-    @FXML
-    private TextField phoneNumberFilterField;
-    @FXML
-    private TextField emailFilterField;
-    @FXML
-    private TextField studentIdFilterField;
-    @FXML
-    private TextField roomNumberFilterField;
-    @FXML
-    private TextField majorFilterField;
-    @FXML
-    private TextField emergencyContactFilterField;
-    @FXML
-    private ComboBox<String> yearFilterComboBox;
-    @FXML
-    private ComboBox<String> genderFilterComboBox;
-    @FXML
-    private FontIcon filterIcon;
-    @FXML
-    private FontIcon sortIcon;
+    private StackPane nameFilterFieldPlaceholder;
 
     /**
-     * Creates a {@code FilterPanel} with the given {@code ObjectProperty<FilterDetails>}.
+     * Creates a {@code FilterPanel} with the given {@code ReadOnlyFilterDetails}.
      */
-    public FilterPanel(ObjectProperty<FilterDetails> filterDetails) {
+    public FilterPanel(ReadOnlyFilterDetails filterDetails, FilterExecutor filterExecutor) {
         super(FXML);
         this.filterDetails = filterDetails;
+        this.filterExecutor = filterExecutor;
         fillInnerParts();
     }
 
@@ -70,9 +50,18 @@ public class FilterPanel extends UiPart<Region> {
                 (SetChangeListener<? super String>) change ->
                         nameFilterField.setKeywords(List.copyOf(filterDetails.getNameKeywords())));
 
-        // Trigger the listener in ModelManager
-        filterDetails.set(newFilterDetails);
+    }
 
-        nameFilterField.clear();
+    private void handleNameKeywordsChanged(List<String> nameKeywords) {
+        // A linked hash set is used to preserve the order of the keywords as entered by the user
+        Set<String> nameKeywordsSet = new LinkedHashSet<>(nameKeywords);
+        FilterDetails newFilterDetails = new FilterDetails(filterDetails);
+        newFilterDetails.setNameKeywords(nameKeywordsSet);
+
+        try {
+            filterExecutor.execute(newFilterDetails);
+        } catch (CommandException e) {
+            // No-op: MainWindow#executeCommand will handle displaying the error message to the user.
+        }
     }
 }
