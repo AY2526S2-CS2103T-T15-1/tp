d0605eeb6f535f06b34672e3cb951f1cc10f6010
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-04-03 16:38:07 +0800

    SampleDataUtil: Refactor sample persons
    Let's:
    * Make sure that the RoomNumber, EmergencyContact,... are in the
    validated format
    * Add TagSet, and each tag is in the validated format
    * Add Demerit Incident List

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -7,6 +7,9 @@ import java.util.List;
 import seedu.address.commons.exceptions.DataLoadingException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.demerit.DemeritIncident;
+import seedu.address.model.demerit.DemeritRule;
+import seedu.address.model.demerit.DemeritRuleCatalogue;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.EmergencyContact;
 import seedu.address.model.person.Name;
@@ -26,23 +29,60 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new StudentId("A0485321Y"), new RoomNumber("4-A"),
-                        new EmergencyContact("98765432"), new Remark(""), new HashMap<>()),
+                    new StudentId("A0485321Y"), new RoomNumber("4-A"),
+                    new EmergencyContact("98765432"), new Remark("My best friend"),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "2"},
+                            new Object[] {TagType.GENDER, "he/him"},
+                            new Object[] {TagType.MAJOR, "Computer Science"}),
+                    getDemeritIncidentList(
+                            new Object[] {31, 1, "Left fan on overnight"},
+                            new Object[] {28, 1, "Common area left untidy"})),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new StudentId("A1123456Z"), new RoomNumber("15-R"),
-                        new EmergencyContact("91234567"), new Remark(""), new HashMap<>()),
+                    new StudentId("A1123456Z"), new RoomNumber("15-R"),
+                    new EmergencyContact("91234567"), new Remark(""),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "1"},
+                            new Object[] {TagType.GENDER, "she/her"},
+                            new Object[] {TagType.MAJOR, "Business"}),
+                    List.of()),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new StudentId("A1246354T"), new RoomNumber("3-D"),
-                        new EmergencyContact("87654321"), new Remark(""), new HashMap<>()),
+                    new StudentId("A1246354T"), new RoomNumber("3-D"),
+                    new EmergencyContact("87654321"), new Remark("Really funny person"),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "4"},
+                            new Object[] {TagType.GENDER, "they/them"},
+                            new Object[] {TagType.MAJOR, "Psychology"}),
+                    getDemeritIncidentList(
+                            new Object[] {18, 1, "Quiet-hours visitor warning"})),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new StudentId("A0148321W"), new RoomNumber("10-C"),
-                        new EmergencyContact("12345678"), new Remark(""), new HashMap<>()),
+                    new StudentId("A0148321W"), new RoomNumber("10-C"),
+                    new EmergencyContact("12345678"), new Remark(""),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "3"},
+                            new Object[] {TagType.GENDER, "he/him"},
+                            new Object[] {TagType.MAJOR, "Mechanical Engineering"}),
+                    getDemeritIncidentList(
+                            new Object[] {21, 2, "Repeated excessive noise"},
+                            new Object[] {25, 1, "Corridor obstruction"})),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new StudentId("A1436528Q"), new RoomNumber("5-B"),
-                        new EmergencyContact("56789012"), new Remark(""), new HashMap<>()),
+                    new StudentId("A1436528Q"), new RoomNumber("5-B"),
+                    new EmergencyContact("56789012"), new Remark(""),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "2"},
+                            new Object[] {TagType.GENDER, "he/him"},
+                            new Object[] {TagType.MAJOR, "Information Security"}),
+                    List.of()),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                        new StudentId("A0246835Z"), new RoomNumber("12-D"),
-                        new EmergencyContact("23456789"), new Remark(""), new HashMap<>())
+                    new StudentId("A0246835Z"), new RoomNumber("12-D"),
+                    new EmergencyContact("23456789"), new Remark("Allergic to shellfish"),
+                    getTagSet(
+                            new Object[] {TagType.YEAR, "5"},
+                            new Object[] {TagType.GENDER, "he/him"},
+                            new Object[] {TagType.MAJOR, "Law"}),
+                    getDemeritIncidentList(
+                            new Object[] {26, 1, "Unauthorized appliance found"},
+                            new Object[] {26, 2, "Repeated unauthorized appliance use"}))
         };
     }
 
